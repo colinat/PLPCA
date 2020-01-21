ebf2aa6630e5fce7e804e74667344450ac145986
Nelson Low <48354913+nelsonljs@users.noreply.github.com>
2020-01-21 23:23:59 +0800

    Update

--- NLP_nelson.py
+++ NLP_nelson.py
@@ -19,21 +19,12 @@ doc = nlp(u'Rats are various medium-sized, long-tailed rodents.')
 #displacy.render(doc, style='dep',jupyter=True)
 
 #open localhost:5000
-displacy.serve(doc, style="dep")
+#displacy.serve(doc, style="dep")
 #displacy.serve(nlp("Larry Page founded Google"), style="ent")
 
 #displacy.serve(doc, style="dep")
 
 #doc = nlp("Larry Page founded Google")
 #displacy.serve(doc, style="ent")
+    
 
-import pandas as pd
-
-mydata = pd.read_csv("datasets/womens_clothing.csv", nrows=20)
-sampletext = mydata['Review Text'][2]
-
-nlp.add_pipe(nlp.create_pipe('sentencizer'))
-doc = nlp(sampletext)
-sentences = [sent.string.strip() for sent in doc.sents]
-
-mytokens = [token.text for token in doc]
---
+++ Transcript_Parser.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 21 23:21:18 2020
+
+@author: nelso
+"""
+import spacy
+nlp = spacy.load("en_core_web_sm")
+
+import pandas as pd
+import os
+
+directory = os.fsencode('datasets/transcripts')
+
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    if filename.endswith(".txt"): 
+        myfile = os.path.join('datasets/transcripts/', filename)
+        with open(myfile, 'r') as file:
+            mydata = file.readlines()
+        
+        sentences = []
+        mywords = []
+        lemmas = []
+        mytags = []
+        myshape = []
+        
+        for lines in mydata:
+            doc = nlp(lines)
+            for sent in doc.sents:
+                sentences.append(sent.string.strip())
+#            mytokens = [token.text for token in doc]
+#            print(mytokens)
+            for token in doc:
+#            print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
+#            token.shape_, token.is_alpha, token.is_stop)
+                mywords.append(token.text)
+                lemmas.append(token.lemma_)
+                mytags.append(token.tag_)
+                myshape.append(token.shape_)
+        
+        mydf = pd.DataFrame({'Word': mywords,
+                             'Lemma': lemmas,
+                             'POSTag': mytags,
+                             'Shape': myshape})
+        mysentences = pd.DataFrame({'Sentences': sentences})
+
+        mydf.to_csv(''.join(['datasets/transcripts/',filename[:-4],'.csv']))
+        mysentences.to_csv(''.join(['datasets/transcripts/',filename[:-4],'-sentences.csv']))
+    else:
+        continue

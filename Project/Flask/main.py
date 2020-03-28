2a6637a0a0e2c23d243f0072f70710a85d9e5856
Mritunjay <mritu301@gmail.com>
2020-03-29 02:23:23 +0800

    Updated the Flask main.py

--- Project/Flask/main.py
+++ Project/Flask/main.py
@@ -14,9 +14,15 @@ import os
 
 # create the application object 
 app = Flask(__name__)
+
 flag = False
 
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+
+# inbuilt function which takes error as parameter 
+@app.errorhandler(404)
+def not_found(e): 
+  return render_template("error.html", content=["Oops Something Wrong...!!!"])
 
 @app.route('/Upload', methods=['GET', 'POST'])
 @app.route("/", methods=['GET', 'POST'])
@@ -27,8 +33,8 @@ def Upload():
     print("ROOT_PATH - ", ROOT_PATH)
     print("request.method - ", request.method)
     if request.method == 'POST':
-        return render_template("error.html")
-    return render_template("home.html", content=[" "])
+        return render_template("error.html", content=["Oops Something Wrong...!!!"])
+    return render_template("home.html", content=["Upload the Transcript file for Sentiment Analysis"])
 
 @app.route("/upload", methods=['POST'])
 def upload():
@@ -49,48 +55,48 @@ def upload():
             """
             flag = poly.plotAllAspects(folderLocation) 
             print(flag)
-            return render_template("sales.html", content=["for Aspect - Sales"])
+            return render_template("sales.html", content=["Transcript's Sentiment Analysis for Aspect - Sales"])
     else:
-        render_template("error.html")
+        render_template("error.html", content=["Oops Something Wrong...!!!"])
 
 @app.route('/OpCost')
 def OpCost():
-    return render_template('opCost.html', content=["for Aspect - Operational Cost"]) 
+    return render_template('opCost.html', content=["Transcript's Sentiment Analysis for Aspect - Operational Cost"]) 
 
 @app.route('/Sales')
 def Sales():
-    return render_template('sales.html', content=["for Aspect - Sales"]) 
+    return render_template('sales.html', content=["Transcript's Sentiment Analysis for Aspect - Sales"]) 
 
 @app.route('/ProductServices')
 def ProductServices():
-    return render_template('productServices.html', content=["for Aspect - Product Services"]) 
+    return render_template('productServices.html', content=["Transcript's Sentiment Analysis for Aspect - Product Services"]) 
 
 @app.route('/Debt')
 def Debt():
-    return render_template('debt.html', content=["for Aspect - Debt"]) 
+    return render_template('debt.html', content=["Transcript's Sentiment Analysis for Aspect - Debt"]) 
 
 @app.route('/Earnings')
 def Earnings():
-    return render_template('earnings.html', content=["for Aspect - Earnings"]) 
+    return render_template('earnings.html', content=["Transcript's Sentiment Analysis for Aspect - Earnings"]) 
 
 @app.route('/Acquisitions')
 def Acquisitions():
-    return render_template('acquisitions.html', content=["for Aspect - Acquisitions"]) 
+    return render_template('acquisitions.html', content=["Transcript's Sentiment Analysis for Aspect - Acquisitions"]) 
 
 @app.route('/Competition')
 def Competition():
-    return render_template('competition.html', content=["for Aspect - Competition"]) 
+    return render_template('competition.html', content=["Transcript's Sentiment Analysis for Aspect - Competition"]) 
 
 @app.route('/OpRisks')
 def OpRisks():
-    return render_template('opRisks.html', content=["for Aspect - Operational Risks"])  
+    return render_template('opRisks.html', content=["Transcript's Sentiment Analysis for Aspect - Operational Risks"])  
 
 @app.route("/About")
 def About():
-    return render_template("about.html", content=[" "])
+    return render_template("about.html", content=["About Transcript's Sentiment Analysis and its Team !!!"])
 
 # start the server with the 'run()' method
 if __name__ == "__main__":
-    app.run(host = "0.0.0.0", port = 7777)
+    app.run(host = "127.0.0.1", port = 7777)
     #TEMPLATES_AUTO_RELOAD = True
     app.secret_key = 'super_secret_key'
